3044e4a6e6c486248b75d0e91bc892918843d0c9
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-01 02:38:06 +0200

    Initial commit

---
+++ src/test/java/de/flowsuite/mailflow/microservices/MailflowMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.flowsuite.mailflow.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailflowMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
