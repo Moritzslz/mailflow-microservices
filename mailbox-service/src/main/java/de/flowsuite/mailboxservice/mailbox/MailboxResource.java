f730d9c43bd2f230120267b4be00a1ca8fd2f420
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-04 04:01:07 +0200

    Implemented Resource for notifying

--- mailbox-service/src/main/java/de/flowsuite/mailboxservice/mailbox/MailboxResource.java
+++ mailbox-service/src/main/java/de/flowsuite/mailboxservice/mailbox/MailboxResource.java
@@ -1,8 +1,29 @@
 package de.flowsuite.mailboxservice.mailbox;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import de.flowsuite.mailflow.common.entity.User;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/notify")
-class MailboxResource {}
+@RequestMapping("/notify/user")
+class MailboxResource {
+
+    private final MailboxService mailboxService;
+
+    public MailboxResource(MailboxService mailboxService) {
+        this.mailboxService = mailboxService;
+    }
+
+    @PostMapping
+    ResponseEntity<Void> onUserCreated(@RequestBody @Valid User user) {
+        mailboxService.onUserCreated(user);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    ResponseEntity<Void> onUserUpdated(@RequestBody @Valid User user) {
+        mailboxService.onUserUpdated(user);
+        return ResponseEntity.ok().build();
+    }
+}
