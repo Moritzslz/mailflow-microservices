85cbcb3f2350857fd1886404fdae32a5a02d326d
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-06 04:42:39 +0200

    Small improvement

--- mailbox-service/src/main/java/de/flowsuite/mailboxservice/exception/MailboxNotFoundException.java
+++ mailbox-service/src/main/java/de/flowsuite/mailboxservice/exception/MailboxNotFoundException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
-public class MailboxNotFoundException extends RuntimeException {
+public class MailboxNotFoundException extends MailboxException {
 
     public MailboxNotFoundException(long userId) {
-        super(String.format("No active mailbox connection found for user %s", userId));
+        super(String.format("No active mailbox connection found for user %s", userId) ,false);
     }
 }
