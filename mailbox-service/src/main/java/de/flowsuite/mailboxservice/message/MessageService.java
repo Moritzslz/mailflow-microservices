0ed35bb3613f3f0ace060bfbcd7bb88f61721512
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-08 22:31:28 +0200

    Removed TODO

--- mailbox-service/src/main/java/de/flowsuite/mailboxservice/message/MessageService.java
+++ mailbox-service/src/main/java/de/flowsuite/mailboxservice/message/MessageService.java
@@ -3,4 +3,18 @@ package de.flowsuite.mailboxservice.message;
 import org.springframework.stereotype.Service;
 
 @Service
-class MessageService {}
+class MessageService {
+
+    private final MessageProcessor processor;
+    private final MessageResponseHandler responseHandler;
+
+    public MessageService(MessageProcessor processor, MessageResponseHandler responseHandler) {
+        this.processor = processor;
+        this.responseHandler = responseHandler;
+    }
+
+    public void processMessage(Message message) {
+        this.processor.processMessage(message, this.responseHandler);
+    }
+
+}
