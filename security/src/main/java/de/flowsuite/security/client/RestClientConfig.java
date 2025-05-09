7e5a27008d36c35e1b5c7dc58232376dfdff209c
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-09 13:46:02 +0200

    Added llm service rest client

--- security/src/main/java/de/flowsuite/security/client/RestClientConfig.java
+++ security/src/main/java/de/flowsuite/security/client/RestClientConfig.java
@@ -8,20 +8,31 @@ import org.springframework.web.client.RestClient;
 @Configuration
 class RestClientConfig {
 
-    private final String baseUrl;
+    private final String apiBaseUrl;
+    private final String llmServiceBaseUrl;
     private final JwtRestClientInterceptor jwtRestClientInterceptor;
 
     RestClientConfig(
-            @Value("${mailflow.api.base-url}") String baseUrl,
+            @Value("${mailflow.api.base-url}") String apiBaseUrl,
+            @Value("${mailflow.llm-service.base-url}") String llmServiceBaseUrl,
             JwtRestClientInterceptor jwtRestClientInterceptor) {
-        this.baseUrl = baseUrl;
+        this.apiBaseUrl = apiBaseUrl;
+        this.llmServiceBaseUrl = llmServiceBaseUrl;
         this.jwtRestClientInterceptor = jwtRestClientInterceptor;
     }
 
     @Bean
-    public RestClient restClient() {
+    public RestClient apiRestClient() {
         return RestClient.builder()
-                .baseUrl(baseUrl)
+                .baseUrl(apiBaseUrl)
+                .requestInterceptor(jwtRestClientInterceptor)
+                .build();
+    }
+
+    @Bean
+    public RestClient llmServiceRestClient() {
+        return RestClient.builder()
+                .baseUrl(llmServiceBaseUrl)
                 .requestInterceptor(jwtRestClientInterceptor)
                 .build();
     }
