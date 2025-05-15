8749726fef965eb186d4ee0e4e1ee1828681c613
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-16 00:02:09 +0200

    Logging

--- shared/src/main/java/de/flowsuite/shared/exception/ExceptionManager.java
+++ shared/src/main/java/de/flowsuite/shared/exception/ExceptionManager.java
@@ -2,7 +2,9 @@ package de.flowsuite.shared.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ExceptionManager {
 
     private static final Logger LOG = LoggerFactory.getLogger(ExceptionManager.class);
@@ -15,12 +17,12 @@ public class ExceptionManager {
         if (e instanceof ServiceException exception) {
             if (exception.shouldNotifyAdmin()) {
                 if (log) {
-                    LOG.error("Mailbox exception occurred. Notifying admin. Error:", exception);
+                    LOG.error("Service exception occurred. Notifying admin. Error:", exception);
                 }
                 // TODO: Notify admin
             } else if (log) {
                 LOG.warn(
-                        "Handled mailbox exception (no admin notification required): {}",
+                        "Handled service exception (no admin notification required): {}",
                         exception.getMessage());
             }
         } else {
