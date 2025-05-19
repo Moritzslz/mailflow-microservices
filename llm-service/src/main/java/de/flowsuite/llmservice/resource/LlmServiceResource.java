9134f2193836348a32174e54f7181b9a1c545fe7
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-19 13:41:22 +0200

    Spotless

--- llm-service/src/main/java/de/flowsuite/llmservice/resource/LlmServiceResource.java
+++ llm-service/src/main/java/de/flowsuite/llmservice/resource/LlmServiceResource.java
@@ -4,15 +4,12 @@ import de.flowsuite.llmservice.service.LlmService;
 import de.flowsuite.mailflow.common.dto.LlmServiceRequest;
 import de.flowsuite.mailflow.common.entity.Customer;
 import de.flowsuite.mailflow.common.entity.MessageCategory;
-import de.flowsuite.mailflow.common.entity.User;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 class LlmServiceResource {
