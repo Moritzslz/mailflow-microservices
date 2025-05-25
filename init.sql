bd4354973046b34da87deeeee07ffe8d6e68ae32
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-25 12:27:29 +0200

    Removed table declaration since that is handled by langchain

--- init.sql
+++ init.sql
@@ -1,9 +1,1 @@
 CREATE EXTENSION IF NOT EXISTS vector;
-CREATE TABLE embeddings (
-    id BIGSERIAL PRIMARY KEY,
-    customer_id BIGSERIAL,
-    user_id BIGSERIAL,
-    text TEXT,
-    meta_data JSONB,
-    embedding vector(1536)
-);
