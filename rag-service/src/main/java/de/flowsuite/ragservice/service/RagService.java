d24d0fefa370b6228f8ac6da2ee3e75910522425
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-27 03:10:28 +0200

    Migrated to use the new, common, ApiClient

--- rag-service/src/main/java/de/flowsuite/ragservice/service/RagService.java
+++ rag-service/src/main/java/de/flowsuite/ragservice/service/RagService.java
@@ -2,32 +2,28 @@ package de.flowsuite.ragservice.service;
 
 import static de.flowsuite.mailflow.common.util.Util.BERLIN_ZONE;
 
+import de.flowsuite.mailflow.common.client.ApiClient;
 import de.flowsuite.mailflow.common.entity.Customer;
 import de.flowsuite.mailflow.common.entity.RagUrl;
 import de.flowsuite.mailflow.common.exception.IdConflictException;
 import de.flowsuite.ragservice.agent.RagAgent;
 import de.flowsuite.shared.exception.ExceptionManager;
-import de.flowsuite.shared.exception.ServiceException;
 
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.store.embedding.EmbeddingMatch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
 
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -37,19 +33,12 @@ import javax.sql.DataSource;
 public class RagService {
 
     private static final Logger LOG = LoggerFactory.getLogger(RagService.class);
-    private static final String LIST_CUSTOMERS_URI = "/customers";
-    private static final String GET_CUSTOMERS_URI = "/customers/{customerId}";
-    private static final String PUT_CUSTOMERS_URI = "/customers/{customerId}";
-    private static final String LIST_RAG_URLS_URI = "/customers/{customerId}/rag-urls";
-    private static final String PUT_RAG_URLS_URI = "/customers/{customerId}/rag-urls/{id}";
-    private static final CountDownLatch restartLatch = new CountDownLatch(1);
-    private static final long RESTART_DELAY_MILLISECONDS = 5000;
 
     private static final ConcurrentHashMap<Long, RagAgent> ragAgents = new ConcurrentHashMap<>();
     private static final ConcurrentHashMap<Long, List<RagUrl>> ragUrls = new ConcurrentHashMap<>();
 
     private final boolean debug;
-    private final RestClient apiRestClient;
+    private final ApiClient apiClient;
     private final ExceptionManager exceptionManager;
     private final CrawlingService crawlingService;
     private final DataSource dataSource;
@@ -57,12 +46,12 @@ public class RagService {
 
     public RagService(
             @Value("${langchain.debug}") boolean debug,
-            @Qualifier("apiRestClient") RestClient apiRestClient,
+            ApiClient apiClient,
             ExceptionManager exceptionManager,
             CrawlingService crawlingService,
             DataSource dataSource) {
         this.debug = debug;
-        this.apiRestClient = apiRestClient;
+        this.apiClient = apiClient;
         this.exceptionManager = exceptionManager;
         this.crawlingService = crawlingService;
         this.dataSource = dataSource;
@@ -72,40 +61,8 @@ public class RagService {
     void startRagService() {
         LOG.info("Starting mailbox service");
 
-        List<Customer> customers = null;
-
-        try {
-            // Blocking request
-            customers =
-                    apiRestClient
-                            .get()
-                            .uri(LIST_CUSTOMERS_URI)
-                            .retrieve()
-                            .body(new ParameterizedTypeReference<List<Customer>>() {});
-        } catch (Exception e) {
-            exceptionManager.handleException(
-                    new ServiceException("Failed to fetch customers", e, true));
-        }
-
-        if (customers == null || customers.isEmpty()) {
-            if (restartLatch.getCount() == 1) {
-                restartLatch.countDown();
-                LOG.error(
-                        "No customers found. Restarting once in {} seconds.",
-                        (float) RESTART_DELAY_MILLISECONDS / 1000);
-                try {
-                    Thread.sleep(RESTART_DELAY_MILLISECONDS);
-                } catch (InterruptedException ie) {
-                    Thread.currentThread().interrupt();
-                }
-                startRagService();
-            } else {
-                ServiceException serviceException =
-                        new ServiceException("No customers found. Giving up.", true);
-                exceptionManager.handleException(serviceException);
-            }
-            return;
-        }
+        // Blocking request
+        List<Customer> customers = apiClient.listCustomers();
 
         LOG.info("Received {} customers from API", customers.size());
 
@@ -118,25 +75,30 @@ public class RagService {
         }
     }
 
-    // spotless:off
+    // TODO important note: for this to work the service needs to be restarted daily
     private void submitDailyCrawlTasks(List<Customer> customers) {
         for (Customer customer : customers) {
             LOG.info("Submitting daily crawl task for customer {}", customer.getId());
 
             if (customer.getNextCrawlAt() == null
-                    || ((customer.getNextCrawlAt().toLocalDate().isEqual(LocalDate.now(BERLIN_ZONE)))
-                    && !customer.getLastCrawlAt().toLocalDate().isEqual(LocalDate.now(BERLIN_ZONE)))) {
+                    || ((customer.getNextCrawlAt()
+                                    .toLocalDate()
+                                    .isEqual(LocalDate.now(BERLIN_ZONE)))
+                            && !customer.getLastCrawlAt()
+                                    .toLocalDate()
+                                    .isEqual(LocalDate.now(BERLIN_ZONE)))) {
                 List<RagUrl> ragUrls = getOrFetchRagUrls(customer.getId());
                 RagAgent ragAgent = getOrCreateRagAgent(customer);
                 ragAgent.clear();
-                //ragServiceExecutor.submit(() -> performCrawlForCustomer(customer, ragUrls, ragAgent));
-                performCrawlForCustomer(customer, ragUrls, ragAgent);
+                ragServiceExecutor.submit(
+                        () -> performCrawlForCustomer(customer, ragUrls, ragAgent));
             } else {
-                LOG.info("Skipping customer {} as it's next crawl is in the future", customer.getId());
+                LOG.info(
+                        "Skipping customer {} as it's next crawl is in the future",
+                        customer.getId());
             }
         }
     }
-    // spotless:on
 
     public void triggerOnDemandCrawl(long customerId, Customer customer) {
         if (!customer.getId().equals(customerId)) {
@@ -170,7 +132,8 @@ public class RagService {
             } else {
                 ragUrl.setLastCrawlSuccessful(false);
             }
-            apiRestClient.post().uri(PUT_RAG_URLS_URI).body(ragUrl).retrieve().toBodilessEntity();
+            // Blocking request
+            apiClient.updateRagUrl(ragUrl);
         }
 
         ragAgent.embedAll(crawlingResults);
@@ -179,12 +142,8 @@ public class RagService {
         customer.setNextCrawlAt(
                 customer.getLastCrawlAt().plusDays(customer.getCrawlFrequencyInDays()));
 
-        apiRestClient
-                .put()
-                .uri(PUT_CUSTOMERS_URI, customer.getId())
-                .body(customer)
-                .retrieve()
-                .toBodilessEntity();
+        // Blocking request
+        apiClient.updateCustomer(customer);
     }
 
     public void onRagUrlCreated(long customerId, long id, RagUrl ragUrl) {
@@ -193,12 +152,8 @@ public class RagService {
         }
 
         if (!ragUrls.containsKey(customerId)) {
-            Customer customer =
-                    apiRestClient
-                            .get()
-                            .uri(GET_CUSTOMERS_URI, customerId)
-                            .retrieve()
-                            .body(Customer.class);
+            // Blocking request
+            Customer customer = apiClient.getCustomer(customerId);
 
             if (customer == null) {
                 throw new RuntimeException("Failed to retrieve customer");
@@ -224,18 +179,8 @@ public class RagService {
     }
 
     private List<RagUrl> getOrFetchRagUrls(long customerId) {
-        return ragUrls.computeIfAbsent(customerId, id -> fetchRagUrlsByCustomer(customerId));
-    }
-
-    private List<RagUrl> fetchRagUrlsByCustomer(long customerId) {
-        LOG.debug("Fetching rag urls by customer {}", customerId);
-
-        // Blocking request
-        return apiRestClient
-                .get()
-                .uri(LIST_RAG_URLS_URI, customerId)
-                .retrieve()
-                .body(new ParameterizedTypeReference<List<RagUrl>>() {});
+        return ragUrls.computeIfAbsent(
+                customerId, id -> apiClient.listRagUrls(customerId)); // Blocking request
     }
 
     private RagAgent getOrCreateRagAgent(Customer customer) {
