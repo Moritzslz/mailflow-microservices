dff6c618292ae0592035280868d7fdc207b1f5d3
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-28 17:28:05 +0200

    Added constructor

--- rag-service/src/main/java/de/flowsuite/ragservice/exception/CrawlingException.java
+++ rag-service/src/main/java/de/flowsuite/ragservice/exception/CrawlingException.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 public class CrawlingException extends ServiceException {
 
+    public CrawlingException(String message) {
+        super(message, false);
+    }
+
     public CrawlingException(String message, Throwable cause) {
         super(message, cause, false);
     }
